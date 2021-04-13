bed4385f5cc3492383241827524fc7e062b040b9
dylankrieg <dylankriegman@gmail.com>
2021-04-12 19:03:26 -0600

    Updated testing script

--- test.bash
+++ test.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
-(cd ~/catkin_ws/; source ~/catkin_ws/devel/setup.bash)
-(cd ~/catkin_ws/; catkin_make)
-(cd ~/catkin_ws/src/map_data_structure/launch/; . ~/catkin_ws/devel/setup.bash)
-(cd ~/catkin_ws/src/map_data_structure/launch/; roslaunch map_data_structure map_data_structure.launch)
+source /opt/ros/noetic/setup.bash;
+cd ~/catkin_ws/; catkin_make;
+cd ~/catkin_ws/;
+source devel/setup.bash;
+roscd map_data_structure/launch;
+roslaunch map_data_structure map_data_structure.launch;
